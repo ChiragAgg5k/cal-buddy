23e2f7e407942b6cee50c3296de610cdcaa4bea8
abhi9ab <abhinabdas004@gmail.com>
2024-10-08 09:14:20 +0530

    modularize-dashboard-page

---
+++ src/components/dashboard/quick-actions.tsx
@@ -0,0 +1,20 @@
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function QuickActions() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Quick Actions</CardTitle>
+            </CardHeader>
+            <CardContent className="flex space-x-4">
+                {/* <Button>Schedule Meeting</Button>
+                <Button variant="outline">Create Task</Button>
+                <Button variant="outline">View Calendar</Button> */}
+                <p className="text-center text-gray-500 dark:text-gray-400 text-sm">
+                    Coming soon...
+                </p>
+            </CardContent>
+        </Card >
+    );
+}
